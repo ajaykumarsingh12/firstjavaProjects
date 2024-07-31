2fd9c5a4d6256f0817571f3811f531293995cce3
ajaykumar <ajaykumarsingh5963@gmail.com>
2024-07-31 22:44:45 +0530

    firstProjects

---
+++ src/main/java/com/mycompany/projectfirst/ProjectFirst.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectfirst;
+
+/**
+ *
+ * @author ajays
+ */
+public class ProjectFirst {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
